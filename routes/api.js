943237b63270a65bf549aa287d89606953f922a4
RamiHami <test@test.com>
2022-09-20 21:55:08 +0300

    middleware

---
+++ routes/api.js
@@ -0,0 +1,151 @@
+const express = require('express')
+const router = express.Router()
+const booksUploader = require('../middleware/booksUploader')
+const fs = require('fs')
+const {v4: uuid} = require('uuid')
+
+router.get('/users', (req, res) => {
+    res.status(200).json(store.users)
+})
+
+router.post('/user/login', (req, res) => {
+    const {mail} = req.body
+    const newUser = new User(store.users.length + 1, mail)
+    store.users.push(newUser)
+
+    res.status(201).json(newUser)
+})
+
+router.get('/books', (req, res) => {
+    if (store.books.length) {
+        res.status(200).json(store.books)
+    } else {
+        res.status(200).json('Не создано ни одной книги')
+    }
+})
+
+// router.get('/books/:id', (req, res) => {
+//     const {id} = req.params
+    
+//     store.books.forEach(item => {
+//         if (item.id === id) {
+//             res.status(201).json(item)
+//         }
+//     })
+
+//     res.status(404).json('Такой книги нет.')
+// })
+
+router.post('/books', booksUploader.single('book'), (req, res) => {
+    if (req.file) {
+        const path = req.file.path 
+        const book = fs.readFileSync(path, {encoding: 'utf-8', flag: 'r'})
+        const parsedBook = JSON.parse(book)
+        const newBook = new Book(
+            parsedBook.title,
+            parsedBook.description,
+            parsedBook.authors,
+            parsedBook.favorite,
+            parsedBook.fileCover,
+            parsedBook.fileName,
+            path
+        )
+        // console.log(newBook)
+        store.books.push(newBook)
+        console.log(store.books)
+        res.json(newBook)
+
+        return
+    }
+
+    res.json({
+        error: 'newBook'
+    })
+})
+
+router.get('/books/:id', (req, res) => {
+    const {id} = req.params
+
+    store.books.forEach((item) => {
+        if (item.id === id) {
+            res.download(item.fileBook)
+        } 
+    })
+})
+
+router.put('/books/:id', (req, res) => {
+    const {id} = req.params
+    
+    store.books.forEach((item, key) => {
+        if (item.id === id) {
+            store.books[key] = {
+                ...store.books[key],
+                ...req.body
+            }
+
+            res.status(201).json(store.books[key])
+        }
+    })
+
+    res.status(404).json('Книга не найдена')
+})
+
+router.delete('/books/:id', (req, res) => {
+    const {id} = req.params
+
+    store.books.forEach((item, index) => {
+        if (item.id === id) {
+            store.books.splice(index, 1)
+
+            res.status(201).json('ok')
+        } 
+
+        res.status(404).json('Книга не найдена')
+    })
+})
+
+class Book {
+    constructor(
+        title,
+        description,
+        authors,
+        favorite,
+        fileCover,
+        fileName,
+        fileBook,
+        id = uuid()
+    ) {
+        this.id = id || ''
+        this.title = title || ''
+        this.description = description || ''
+        this.authors = authors || ''
+        this.favorite = favorite || ''
+        this.fileCover = fileCover || ''
+        this.fileName = fileName || ''
+        this.fileBook = fileBook || ''
+    }
+}
+
+class User {
+    constructor(
+        id,
+        mail,
+    ) {
+        this.id = id
+        this.mail = mail
+    }
+}
+
+const store = {
+    books: [
+        
+    ],
+    users: [
+        {
+            id: 1,
+            mail: 'root@root'
+        }
+    ]
+}
+
+module.exports = router
